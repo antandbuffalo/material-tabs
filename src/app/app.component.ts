da65271b9553890e04c4c56379be31a5d9f4e1bc
Jeyabalaji <balaji.jeya@gmail.com>
2020-07-19 16:06:15 +0800

    tab selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-tabs';
+  activeTab = 'home';
+  onTabClick(type: string) {
+    this.activeTab = type;
+    console.log(type);
+  }
 }
--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Tab2homeComponent } from './tab2home/tab2home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Tab2homeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
